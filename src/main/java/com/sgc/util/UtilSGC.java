e87bdf80a47b92d651b657f3cc4b184ea39b10ba
renanramos <renanrramossi@gmail.com>
2018-08-09 08:49:22 -0300

    Corrigido o método getMeses()

--- src/main/java/com/sgc/util/UtilSGC.java
+++ src/main/java/com/sgc/util/UtilSGC.java
@@ -12,30 +12,31 @@ import com.sgc.model.entity.Conta;
 public class UtilSGC {
 
 
-	private Locale ptBr = new Locale("pt", "BR");
-	private DateFormatSymbols dfs = DateFormatSymbols.getInstance(ptBr);
+	private static Locale ptBr = new Locale("pt", "BR");
+	private static DateFormatSymbols dfs = DateFormatSymbols.getInstance(ptBr);
 	private Map<String, Integer> meses = new HashMap<String, Integer>();	
-	private List<String> mesesList =  Arrays.asList(dfs.getMonths()).subList(0, 12);	
+	public static List<String> mesesList =  Arrays.asList(dfs.getMonths()).subList(0, 12);	
 
+	//efetua o cálculo total das contas do período inicial
 	public double calculaTotal(List<Conta> conta){
 		double total = 0;
 		
 		for(Conta c : conta){
 			total += c.getValor();
-		}
-				
+		}				
 		return total;
 	}
 
+	//obtém o índice do mes através do nome
 	public int getMes(String mes){
 		for(int i = 0; i < mesesList.size(); i++){			
 			meses.put(mesesList.get(i), Integer.valueOf(i)+1);			
 		}
 		return meses.get(mes);
-	}	
+	}
 	
 	public List<String> getAllMeses(){
 		return mesesList;
 	}
-	
+		
 }
